09931ffe73adaf670f546f758854935a0f8ecaff
HiraAleem <64699503+HiraAleem@users.noreply.github.com>
2020-06-07 17:06:20 +0500

    hi

---
+++ app.js
@@ -0,0 +1,214 @@
+//ASSIGHMENT1
+alert("Assalamualikum viwers")
+
+
+alert("Error!Please enter a valid password.")
+alert("Welcome to js land...\nHappy Coding")
+alert("welcome to js land...")
+alert("Happy Coding\n prevent this page for creating additonal dialogs")
+var a="Hello...I can run js through my web browser's console";
+console.log(a);
+
+
+//ASSIGNMENT2
+
+var username="HIRA";
+alert(username)
+var myname="HIRA ALEEM";
+alert(myname)
+var message="Hello world";
+alert( message)
+var student="jhone Doe";
+alert(student)
+var student="15 years old";
+alert(student)
+var student="CERTIFIED MOBILE APPLICATION DEVELOPMENT";
+alert(student)
+var js=" pizza\n pizz\n piz\n pi\n p";
+alert(js)
+var email="my email address is";
+var emailaddress=" example@example.com";
+var email1=email+emailaddress;
+alert(email1)
+var book=" I am trying to lern from book A smarter way to learn JavaScript";
+alert(book)
+document.write('<h1>yah!I can write Html content through javascript</h1>');
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design) 
+
+
+//ASSIGNMENT3
+
+var age="I am 15 years old";
+alert(age)
+var n=[100];
+for(var i=0; i<n;i++)
+{
+   alert("you  have visited this site" +" "  + i  +" " + "times")
+}
+var birthyear="My birth year is 1998";
+document.write(birthyear)
+var visitor='<p><b>John Doe</b> ordered <b> 5 T-shirt</b>(s) on XYZ Clothing store.</p>';
+document.write(visitor)
+
+
+  //ASSIGNMENT4
+
+
+var person = "ALI", carName = "civic", price = 20000;
+document.write(person+carNam+price)
+    //LEGAL:
+var name="Ali";
+var $dollar=20;
+var _nameOfBrand="khadi";
+var c=8;
+var firstLegalChar= "1";
+document.write(name  +" "+ dollar$+" "+ nameOfBrand+" "+ c+" "+ firstLegalChar)
+
+//ILLEGAL:
+
+var a=("*qutation is illegal for use while declaring \n*sapce is illegal\n*sapce is illegal\n*use % between the are illegal\n*use number before words are illegal");//console.log(a)
+var a="<h1>Rules for naming JS variables</h1>";
+document.write(a)
+var b="<p>Variable names can only contain number, $,and _.For example $my_1stVariable</p>";
+document.write(b)
+var c="<p>Variables must begin with a letter,$or_. For example $name, _name or name</p>"
+document.write(c)
+var d="<p> Variable names are case sentitive</p>";
+document.write(d)
+var e="<p> Variable names should not be JS keyword</p>";
+document.write(e)
+
+
+//ASSIGNMENT5
+
+var a=5;
+var b=3;
+var c=5+3;
+document.write("sum of 5 and 3 is"+c)
+
+var a=5;
+var b=3;
+var c=5-3;
+document.write("sub of 5 and 3 is"+c)
+
+var a=5;
+var b=3;
+var c=5*3;
+document.write("MUL of 5 and 3 is"+c)
+
+var a=5;
+var b=3;
+var c=5/3;
+document.write("divide of 5 and 3 is"+c)
+
+var a=5;
+var b=3;
+var c=5%3;
+document.write("modulus of 5 and 3 is"+c)
+//QUESTION2
+var a="<p>value after declariation is undefined</p>";
+document.write(a)
+var b=5;
+document.write("Initial value:"+b)
+document.write("<br>")
+var b=5;
+var c=++b;
+document.write("value after incrementis:"+c)
+document.write("<br>")
+var b=5;
+var d=++b+7;
+document.write("value after addition is:"+d)
+document.write("<br>")
+var b=5;
+var d=++b+7;
+var e=--d;
+document.write("value after decrement is:"+e)
+document.write("<br>")
+var b=5;
+var d=++b+7;
+var e=--d;
+var f=e%3;
+document.write("value after decrement is:"+f)
+//COST
+var cost=600;
+var c= cost++ +cost++ +cost++ +cost++ + cost-10 ;
+document.write("Total cost of 5 tickets to a movie is"+c+"PKR")
+ var a="Table of 4"
+ document.write(a)
+ //TABLE
+for(var i=1;i<=10;i++)
+{
+    document.write("4"+"x"+i+"="+4*i+"<br>")
+}
+//  TEMPERATURE
+var C=25;
+var F=(C*9/5)+32;
+var message=C+'\xB0C is '  +  F + ' \xB0F.' ;
+document.write(message)
+document.write("<br>")
+
+var F=70;
+var C=(F-32)*5/9;
+var message=F+'\xB0F is'  +  C + '\xB0C.' ;
+document.write(message)
+//SHOPPING CART
+ document.write(" Price of item 1 is 650")
+ document.write("<br>")
+document.write(" quantity of item 1 is 3")
+ document.write("<br>")
+ document.write(" quantity of item 1 is 3")
+ document.write("<br>")
+ document.write(" Price of item 2 is 100")
+ document.write("<br>")
+ document.write(" quantity of item 2 is 7")
+ document.write("<br>")
+ document.write("Shipping charges is 100")
+ document.write("<br>")
+
+var price1=650;
+var total1=price1+price1+price1;
+var price2=100;
+var total2=price2+price2+price2+price2+price2+price2+price2
+var shipping=100;
+var grandtotal=total1+total2+shipping;
+document.write( "Total cost of your order is"+grandtotal);
+
+//MARKSHEET
+var TOTAL=980;
+document.write("Total marks is"+TOTAL)
+document.write("<br>")
+
+var math=402;
+var opp=402;
+var total=math+opp;
+document. write("mark obtained"+total)
+document.write("<br>")
+
+ var per=(total/TOTAL)*100;
+ document.write("percentage is"+per)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
